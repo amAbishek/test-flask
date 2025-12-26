cb7e33ee59df6d9174c007fe3af431209342b0cc
amAbishek <110091362+amAbishek@users.noreply.github.com>
2025-12-27 00:00:18 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,19 @@
+import unittest
 from app import app
 
-def test_home_page_status():
-    client = app.test_client()
-    response = client.get("/")
-    assert response.status_code == 200
+class FlaskAppTestCase(unittest.TestCase):
+    def setUp(self):
+        # Set up a test client before each test
+        self.app = app.test_client()
+        self.app.testing = True
 
-def test_home_page_content():
-    client = app.test_client()
-    response = client.get("/")
-    assert b"<html" in response.data
+    def test_home_page_status_code(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_content(self):
+        response = self.app.get('/')
+        self.assertIn(b"<title>", response.data)  # Change to a string inside index.html
+
+if __name__ == '__main__':
+    unittest.main()
