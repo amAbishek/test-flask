22c1dc69eb8469d5b7ec0189c84c0d5dc1c6bc61
amAbishek <110091362+amAbishek@users.noreply.github.com>
2025-12-27 00:13:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
-# Use official Python image
-FROM python:3.10-slim
+from flask import Flask, render_template
+print('hello')
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template('index.html')
 
-# Set working directory
-WORKDIR /app
-
-# Copy app files
-COPY requirements.txt requirements.txt
-RUN pip install --no-cache-dir -r requirements.txt
-
-COPY . .
-
-# Expose port Flask runs on
-EXPOSE 5000
-
-# Run the application
-CMD ["python", "app.py"]
+if __name__ == "__main__":
+    app.run(debug=True)
